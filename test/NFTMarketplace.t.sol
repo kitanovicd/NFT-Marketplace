776ac2bedcc290509856446cc4f7a1bca64b53f8
kitanovicd <kitanovic.danilo@outlook.com>
2023-06-15 09:17:22 +0200

    Improved unit tests

--- test/NFTMarketplace.t.sol
+++ test/NFTMarketplace.t.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 import {Test} from "forge-std/Test.sol";
 import {NFTMarketplace} from "../src/NFTMarketplace.sol";
 import {MintableERC721} from "./mock/MintableERC721.sol";
+import {NotAllowed} from "../src/NFTMarketplace.sol";
+import {NotEnoughFunds} from "../src/NFTMarketplace.sol";
 
 contract NFTMarketplaceTest is Test {
     address public seller;
@@ -53,6 +55,8 @@ contract NFTMarketplaceTest is Test {
         nftMarketplace.listItem(address(mintableERC721), 1, 10 ether);
         nftMarketplace.unlistItem(address(mintableERC721), 1);
 
+        vm.stopPrank();
+
         assertEq(mintableERC721.ownerOf(1), seller);
         assertEq(
             nftMarketplace.getItemForSale(address(mintableERC721), 1).seller,
@@ -62,5 +66,64 @@ contract NFTMarketplaceTest is Test {
             nftMarketplace.getItemForSale(address(mintableERC721), 1).price,
             0
         );
+    }
+
+    function testUnlistItemRevertNotAllowed() public {
+        vm.startPrank(seller);
+
+        mintableERC721.approve(address(nftMarketplace), 1);
+        nftMarketplace.listItem(address(mintableERC721), 1, 10 ether);
+
+        vm.stopPrank();
+        vm.startPrank(buyer);
+
+        vm.expectRevert(NotAllowed.selector);
+        nftMarketplace.unlistItem(address(mintableERC721), 1);
+        vm.stopPrank();
+    }
+
+    function testBuyItem() public {
+        vm.startPrank(seller);
+        mintableERC721.approve(address(nftMarketplace), 1);
+        nftMarketplace.listItem(address(mintableERC721), 1, 10 ether);
+        vm.stopPrank();
+
+        vm.startPrank(buyer);
+
+        uint256 buyerBalanceBefore = buyer.balance;
+        uint256 nftMarketplaceBalanceBefore = address(nftMarketplace).balance;
+
+        nftMarketplace.buyItem{value: 10 ether}(address(mintableERC721), 1);
+        vm.stopPrank();
+
+        uint256 buyerBalanceAfter = buyer.balance;
+        uint256 nftMarketplaceBalanceAfter = address(nftMarketplace).balance;
+
+        assertEq(
+            nftMarketplaceBalanceAfter,
+            nftMarketplaceBalanceBefore + 10 ether
+        );
+        assertEq(buyerBalanceAfter, buyerBalanceBefore - 10 ether);
+        assertEq(mintableERC721.ownerOf(1), buyer);
+        assertEq(
+            nftMarketplace.getItemForSale(address(mintableERC721), 1).seller,
+            address(0)
+        );
+        assertEq(
+            nftMarketplace.getItemForSale(address(mintableERC721), 1).price,
+            0
+        );
+    }
+
+    function testBuyRevertNotEnoughFunds() public {
+        vm.startPrank(seller);
+        mintableERC721.approve(address(nftMarketplace), 1);
+        nftMarketplace.listItem(address(mintableERC721), 1, 10 ether);
+        vm.stopPrank();
+
+        vm.startPrank(buyer);
+        vm.expectRevert(NotEnoughFunds.selector);
+        nftMarketplace.buyItem{value: 9 ether}(address(mintableERC721), 1);
+        vm.stopPrank();
     }
 }
